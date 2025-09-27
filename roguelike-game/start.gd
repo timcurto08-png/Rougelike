db8f4a96679770f2e5e79b0ddd62739340545f91
CB2000Skull <coltonjb2@gmail.com>
2025-09-27 16:14:48 -0400

    FI PLEASe

--- roguelike-game/start.gd
+++ roguelike-game/start.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://main2.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
